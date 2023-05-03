acfb7b467648a603a1b949e80319118ccf733569
Douglas Lima <douglas07a@gmail.com>
2023-05-03 13:01:03 -0300

    Atualização menu responsivo

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,18 @@
 function clickMenu () {
-    if (menu.style.display == 'block') {
+    var menu = document.querySelector('#menu')
+
+    if (menu.style.display === 'block') {
         menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
+
+function clickClose () {
+    var menu = document.querySelector('#menu')
+
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block'
     } else {
         menu.style.display = 'none'
     }
